f3aa194b66d38c7fce8b40c82ed5d630bac1f382
sm9171 <sm9171@wisenut.co.kr>
2021-06-25 22:04:28 +0900

    dd

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -1,13 +1,10 @@
 package jpabook.jpashop.domain.item;
 
 
-import jpabook.jpashop.domain.Category;
 import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
-import java.util.ArrayList;
-import java.util.List;
 
 @Entity
 @Inheritance(strategy = InheritanceType.JOINED)
@@ -23,7 +20,4 @@ public abstract class Item {
     private String name;
     private int price;
     private int stockQuantity;
-
-    @ManyToMany(mappedBy = "items")
-    private List<Category> categories = new ArrayList<>();
 }
